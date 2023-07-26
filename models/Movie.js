82c118e1f95f799a53292bf3f9af636471c0b035
arshadiqbal7838 <95671481+arshadiqbal7838@users.noreply.github.com>
2023-07-26 12:25:57 +0530

    Movie.js

--- models/Movie.js
+++ models/Movie.js
@@ -1,30 +1,26 @@
+
 const mongoose = require("mongoose");
 const movieSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  runtime: {
+    type: Number,
+    min: 1,
+    required: true,
+  },
 
-    name : {
-        type: String,
-        required: true,
-        maxLength: 10
-    }
-    ,
-    runtime: {
-        type : Number,
-        min: 1, 
-        max: [5, "movie runtime cannot exceed 5 hrs"],
-        required : true
-    },
+  year: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
 
-    year: {
-        type: String,
-        required:true
-    },
-    image: {
+const Movie = mongoose.model("Movie", movieSchema);
 
-        type: String,
-        required : true
-    }
-})
-
-const Movie = mongoose.Model("Movie", movieSchema);
-
-module.exports = Movie
+module.exports = Movie;
